e001b6cd618c86ccaefdb883d9b4eda5499f8f45
Ivan Danilyuk <werwetsad@gmail.com>
2022-11-06 16:36:53 +0500

    Completed Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,15 +2,41 @@ package com.example.task02;
 
 public class Task02Main {
 
+    public enum Months{
+        январь,
+        февраль,
+        март,
+        апрель,
+        май,
+        июнь,
+        июль,
+        август,
+        сентябрь,
+        октябрь,
+        ноябрь,
+        декабрь
+    }
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         System.out.println(getSeason(-5));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try{
+            if (1 <= monthNumber & monthNumber <= 12){
+                if (monthNumber <= 2 | monthNumber == 12) return "зима";
+                if (monthNumber <= 5) return "весна";
+                if (monthNumber <= 8) return "лето";
+                else return "осень";
+            }
+            else {
+                String exText = String.format("monthNumber %s is invalid, month number should be between 1..12", monthNumber);
+                throw new IllegalArgumentException(exText);
+            }
+        }
+        catch(IllegalArgumentException e){
+            throw e;
+        }
     }
 }
